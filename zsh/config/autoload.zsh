6030e56453f1342aafb45eec2d07d295ea9b986c
john <neongulfbaul@gmail.com>
2023-01-03 06:31:14 +1000

    updates and further simplification

--- zsh/config/autoload.zsh
+++
@@ -1,5 +0,0 @@
-# Enable and initialize ZSH's prompt theme and configuration support.
-# See:
-#   1. http://zsh.sourceforge.net/Doc/Release/User-Contributions.html#Prompt-Themes
-autoload -Uz promptinit && promptinit
-autoload -Uz colors && colors
