12fecbfc8370ca914a3cadd89f959ac2675ec09b
neongulfbaul <neongulfbaul@gmail.com>
2024-12-03 21:23:36 +1100

    adding xmobar part 2

--- config/xmonad.hs
+++ config/xmonad.hs
@@ -343,7 +343,18 @@ myEventHook = mempty
 -- Perform an arbitrary action on each internal state change or X event.
 -- See the 'XMonad.Hooks.DynamicLog' extension for examples.
 --
-myLogHook = return ()
+-- myLogHook = return ()
+
+myLogHook = dynamicLogWithPP xmobarPP
+    { ppOutput = hPutStrLn xmproc0 -- ensure this matches your xmobar pipe
+    , ppTitle = xmobarColor "green" "" . shorten 50
+    , ppCurrent = xmobarColor "yellow" "" . wrap "[" "]"
+    , ppVisible = xmobarColor "lightblue" "" . wrap "(" ")"
+    , ppHidden = xmobarColor "gray" ""
+    , ppHiddenNoWindows = xmobarColor "darkgray" ""
+    , ppUrgent = xmobarColor "red" "" . wrap "!" "!"
+    , ppLayout = xmobarColor "cyan" ""
+    }
 
 ------------------------------------------------------------------------
 -- Startup hook
