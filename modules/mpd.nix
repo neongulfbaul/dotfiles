2e86a7ce7b388ac3d5b72faa237029aa50b1989b
neon gulfbaul <neongulfbaul@gmail.com>
2026-01-12 10:46:30 +1100

    fixing mpd2

--- modules/mpd.nix
+++ modules/mpd.nix
@@ -4,58 +4,33 @@
 with lib;
 
 {
-  options.modules.mpd = {
+  options.modules.mpdModule = {
     enable = mkEnableOption "Enable MPD + ncmpcpp";
   };
 
-  config = mkIf config.modules.mpd.enable {
+  config = mkIf config.modules.mpdModule.enable {
 
-    # MPD
-    programs.mpd = {
-      enable = true;
-      package = pkgs.mpd;
-      config = ''
-        music_directory     "${pkgs.writeText "mpdMusicDir" ""}"
-        playlist_directory  "${pkgs.writeText "mpdPlaylistDir" ""}"
-        db_file             "${pkgs.writeText "mpdDb" ""}"
-        log_file            "${pkgs.writeText "mpdLog" ""}"
-        pid_file            "${pkgs.writeText "mpdPid" ""}"
-        state_file          "${pkgs.writeText "mpdState" ""}"
-        sticker_file        "${pkgs.writeText "mpdStickers" ""}"
+    # Enable MPD service
+    services.mpd.enable = true;
+    services.mpd.musicDirectory = "${config.home.homeDirectory}/Music";
+    services.mpd.dbFile = "${config.home.homeDirectory}/.config/mpd/tag_cache";
+    services.mpd.playlistDirectory = "${config.home.homeDirectory}/.config/mpd/playlists";
+    services.mpd.extraConfig = ''
+      log_file "${config.home.homeDirectory}/.config/mpd/mpd.log"
+      pid_file "${config.home.homeDirectory}/.config/mpd/mpd.pid"
+      state_file "${config.home.homeDirectory}/.config/mpd/mpdstate"
+      sticker_file "${config.home.homeDirectory}/.config/mpd/sticker.sql"
+      audio_output {
+          type "pulse"
+          name "PulseAudio"
+      }
+    '';
 
-        bind_to_address     "127.0.0.1"
-        port                "6600"
+    # Enable ncmpcpp client
+    programs.ncmpcpp.enable = true;
+    programs.ncmpcpp.mpdMusicDir = services.mpd.musicDirectory;
 
-        audio_output {
-            type            "pulse"
-            name            "PulseAudio"
-        }
-
-        zeroconf_enabled     "no"
-        auto_update          "yes"
-      '';
-    };
-
-    # ncmpcpp
-    programs.ncmpcpp = {
-      enable = true;
-      package = pkgs.ncmpcpp;
-      config = ''
-        mpd_host = "127.0.0.1"
-        mpd_port = "6600"
-        visualizer_plugin = "cava"
-        update_interval = 1
-        show_album_art = yes
-        song_browser_sort = "artist"
-        bind = yes
-      '';
-    };
-
-    # Cava visualizer
-    programs.cava = {
-      enable = true;
-      package = pkgs.cava;
-    };
+    # optional: install cava if you want visualizer in ncmpcpp
+    home.packages = [ pkgs.cava ];
   };
 }
-
