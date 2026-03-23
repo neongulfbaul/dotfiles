f1f8a836470f923fa6b56a5b909275a1d31a0e9a
neon gulfbaul <neongulfbaul@gmail.com>
2026-03-24 08:06:04 +1100

    home again

--- modules/home.nix
+++ modules/home.nix
@@ -1,62 +1,64 @@
-# mo# modules/home.nix
 { lib, config, options, inputs, ... }:
 let
-  inherit (lib) mkOption mkOrder mkForce mkAliasDefinitions mapAttrs' nameValuePair types;
+  inherit (lib) mkOption mkOrder mkForce mkAliasDefinitions mapAttrs' nameValuePair types; # [cite: 1]
   
-  mkOpt  = type: default: mkOption { inherit type default; };
-  mkOpt' = type: default: description: mkOption { inherit type default description; };
+  # Helpers for defining options
+  mkOpt  = type: default: mkOption { inherit type default; }; # [cite: 2]
+  mkOpt' = type: default: description: mkOption { inherit type default description; }; # [cite: 3]
 
   cfg  = config.home;
-  user = config.user.name;
+  user = config.user.name; # [cite: 4]
 in {
   imports = [
-    inputs.home-manager.nixosModules.home-manager
+    inputs.home-manager.nixosModules.home-manager # [cite: 4]
   ];
 
   options.home = {
-    file       = mkOpt' types.attrs {} "Files to place directly in $HOME";
-    configFile = mkOpt' types.attrs {} "Files to place in $XDG_CONFIG_HOME";
-    dataFile   = mkOpt' types.attrs {} "Files to place in $XDG_DATA_HOME";
-    fakeFile   = mkOpt' types.attrs {} "Files to place in $XDG_FAKE_HOME";
+    # File aliases for cleaner modules
+    file       = mkOpt' types.attrs {} "Files to place directly in $HOME"; # [cite: 5]
+    configFile = mkOpt' types.attrs {} "Files to place in $XDG_CONFIG_HOME"; # [cite: 6]
+    dataFile   = mkOpt' types.attrs {} "Files to place in $XDG_DATA_HOME"; # [cite: 7]
+    fakeFile   = mkOpt' types.attrs {} "Files to place in $XDG_FAKE_HOME (jail for bad actors)"; # [cite: 8]
 
-    dir       = mkOpt types.str config.user.home;
-    binDir    = mkOpt types.str "${cfg.dir}/.local/bin";
+    # Directory definitions
+    dir       = mkOpt types.str config.user.home; # [cite: 9]
+    binDir    = mkOpt types.str "${cfg.dir}/.local/bin"; # [cite: 10]
     cacheDir  = mkOpt types.str "${cfg.dir}/.cache";
     configDir = mkOpt types.str "${cfg.dir}/.config";
-    dataDir   = mkOpt types.str "${cfg.dir}/.local/share";
+    dataDir   = mkOpt types.str "${cfg.dir}/.local/share"; # [cite: 11]
     stateDir  = mkOpt types.str "${cfg.dir}/.local/state";
-    fakeDir   = mkOpt types.str "${cfg.dir}/.local/user";
+    fakeDir   = mkOpt types.str "${cfg.dir}/.local/user"; # [cite: 11]
   };
 
   config = {
-    # 1. Set XDG vars early for the whole system (Safe for servers)
-    environment.sessionVariables = mkOrder 10 {
+    # Set XDG vars early for the whole system
+    environment.sessionVariables = mkOrder 10 { # [cite: 13]
       XDG_BIN_HOME    = cfg.binDir;
-      XDG_CACHE_HOME  = cfg.cacheDir;
+      XDG_CACHE_HOME  = cfg.cacheDir; # [cite: 14]
       XDG_CONFIG_HOME = cfg.configDir;
       XDG_DATA_HOME   = cfg.dataDir;
       XDG_STATE_HOME  = cfg.stateDir;
-      XDG_FAKE_HOME   = cfg.fakeDir;
+      XDG_FAKE_HOME   = cfg.fakeDir; # [cite: 15]
     };
 
-    # 2. Map fakeFile entries (Empty by default)
-    home.file = mapAttrs' (k: v: nameValuePair "${cfg.fakeDir}/${k}" v) cfg.fakeFile;
+    # Map fakeFile entries into the jail dir
+    home.file = mapAttrs' (k: v: nameValuePair "${cfg.fakeDir}/${k}" v) cfg.fakeFile; # [cite: 16]
 
-    # 3. Home Manager Setup (Always defined, but silent if unused)
     home-manager = {
-      useUserPackages = true;
-      extraSpecialArgs = { inherit inputs; };
+      useUserPackages = true; # [cite: 17]
+      extraSpecialArgs = { inherit inputs; }; # [cite: 18, 19]
 
       users.${user} = {
-        home.stateVersion = config.system.stateVersion; # The "VM Fix"
+        home.stateVersion = config.system.stateVersion; # [cite: 20]
         home.file = mkAliasDefinitions options.home.file;
-        
-        xdg = {
-          configFile = mkAliasDefinitions options.home.configFile;
-          dataFile   = mkAliasDefinitions options.home.dataFile;
-          cacheHome  = mkForce cfg.cacheDir;
+       
+          xdg = {
+          configFile = mkAliasDefinitions options.home.configFile; # [cite: 20]
+          dataFile   = mkAliasDefinitions options.home.dataFile; # [cite: 21]
+          # Force HM to use our defined system paths
+          cacheHome  = mkForce cfg.cacheDir; # [cite: 21]
           configHome = mkForce cfg.configDir;
-          dataHome   = mkForce cfg.dataDir;
+          dataHome   = mkForce cfg.dataDir; # [cite: 22]
           stateHome  = mkForce cfg.stateDir;
         };
       };
